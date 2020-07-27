f66029d8b0fe1c914bb8532698931d51ea213741
tferr <tiago.tferr@gmail.com>
2020-07-27 01:41:39 -0400

    Add contextual commands to console

--- src/main/java/sc/fiji/GraphEditor/editor/EditorConsole.java
+++ src/main/java/sc/fiji/GraphEditor/editor/EditorConsole.java
@@ -3,14 +3,18 @@ package sc.fiji.GraphEditor.editor;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.PrintStream;
 
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class EditorConsole extends JPanel {
+public class EditorConsole extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private final JTextArea ta;
@@ -25,16 +29,54 @@ public class EditorConsole extends JPanel {
 		sOut = System.out;
 		sErr = System.err;
 		taStream = new TextAreaOutputStream(ta);
-		// ta.setMargin(new Insets(10,10,10,10));
 		ta.setFont(new Font(Font.MONOSPACED, Font.PLAIN, ta.getFont().getSize()));
 		final JScrollPane pane = new JScrollPane(ta);
 		pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		add(pane);
+		assignPopupMenu();
 	}
 
-	public void setPreferredWidth(int width) {
-		int cols = Math.max(1, width / 55);
+	private void assignPopupMenu() {
+		final JPopupMenu pMenu = new JPopupMenu();
+		addMenuItem(pMenu, "Cut");
+		addMenuItem(pMenu, "Copy");
+		pMenu.addSeparator();
+		addMenuItem(pMenu, "Clear");
+		pMenu.addSeparator();
+		final JCheckBoxMenuItem jmi = new JCheckBoxMenuItem("Pause");
+		jmi.addItemListener(e -> {
+			if (jmi.isSelected()) restore(); else redirect();
+		});
+		pMenu.add(jmi);
+		ta.setComponentPopupMenu(pMenu);
+	}
+
+	private void addMenuItem(final JPopupMenu pMenu, final String cmd) {
+		final JMenuItem jmi = new JMenuItem(cmd);
+		jmi.addActionListener(this);
+		pMenu.add(jmi);
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case "Cut":
+			ta.cut();
+			break;
+		case "Copy":
+			ta.copy();
+			break;
+		case "Clear":
+			taStream.clear();
+			break;
+		default:
+			throw new IllegalArgumentException("Unrecognized option: " + e);
+		}
+	}
+
+	protected void setPreferredWidth(final int width) {
+		final int cols = Math.max(1, width / 55);
 		setPreferredSize(new Dimension(width, (getComponentCount() * 55 / cols) + 30));
 		revalidate();
 	}
